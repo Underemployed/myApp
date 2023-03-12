a5d4879b4ddac8dd819d73aec90e0a3e50345194
Underemployed <underemployed871@gmail.com>
2023-03-12 08:14:13 +0630

    learning flutter day 1

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:ffi';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  ));
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("AppBar"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent[300],
+    ),
+      body:Center(
+        child:Text("body",
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[900],
+              fontFamily: 'IndieFlower'
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("button"),
+        backgroundColor: Colors.blueAccent[200],
+      ),
+    );
+  }
+}
