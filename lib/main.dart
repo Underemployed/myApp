89dcf959601885294d15daaa320b027b9af9250a
Underemployed <underemployed871@gmail.com>
2023-03-12 16:51:51 +0630

    day 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+
   home: Home(),
   ));
 class Home extends StatelessWidget {
@@ -11,14 +12,40 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var num ='one';
     return Scaffold(
       appBar: AppBar(
         title:Text("AppBar"),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent[300],
+        leading:IconButton(onPressed: (){},icon: Icon(Icons.home),),
+          actions: [IconButton(onPressed:(){},icon:Icon(Icons.call)),IconButton(onPressed: (){}, icon:Icon(Icons.more_vert_sharp))],
     ),
-      body:Center(
-        /*
+      body:Padding(
+        padding: EdgeInsets.all(90.0),
+        child: Text("Bye"),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){},label:Text("CLicl"),icon:Icon(Icons.change_circle_outlined),backgroundColor: Colors.blueAccent[300],
+      ),
+      );
+
+      /*Container(
+        padding: EdgeInsets.symmetric(horizontal:25,vertical: 25),
+        margin: EdgeInsets.all(30.0),
+        child: Text("Hellow"),
+        color: Colors.grey,
+
+
+      ),//container
+
+        child: IconButton(
+          onPressed: (){
+            print("AAAAAAAAAAAAAAAAAAAAA");
+          },
+          icon: Icon(Icons.alternate_email),
+        )
+
+
         child: Text(
           "body",
           style: TextStyle(
@@ -30,18 +57,22 @@ class Home extends StatelessWidget {
         ),
       ),
 
-      */
+
         child:Image(
           image: AssetImage('assets/wood.jpg')
         ),
 
+        child: Icon(
+          Icons.airport_shuttle,
+          color:Colors.lightBlue
+        ),
 
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text("button"),
         backgroundColor: Colors.blueAccent[200],
-      ),
-    );
+      ), */
+
   }
 }
