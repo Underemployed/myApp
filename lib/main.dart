502257f5374bd08877546b15a22b233260c3f32b
Underemployed <underemployed871@gmail.com>
2023-05-06 07:13:33 +0630

    Welcome Screen Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/pages/home_screen.dart';
+import 'package:myapp/pages/welcome_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: Colors.red),
-      home: HomeScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
---
+++ lib/pages/welcome_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 1.6,
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Stack(children: [
+          Stack(children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.6,
+              decoration: BoxDecoration(
+                  color: Color(0xFF674AEF),
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(70))),
+              child: Center(child: Image.asset("images/books.png")),
+            ),
+          ]),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.666,
+              decoration: BoxDecoration(
+                color: Color(0xFF674AEF),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.666,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(topLeft: Radius.circular(70))),
+              child: Column(children: [
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  "Learning is not Everything",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 2,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                  child: Text(
+                    "Do it before you think, because its just a matter of time",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black.withOpacity(0.9),
+                      wordSpacing: 2,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Material(
+                  color: Color(0xFF674AEF),
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 60),
+                      child: Text(
+                        "Let's Start",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )
+              ]),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
